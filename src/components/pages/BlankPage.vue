28b9b9e163449719fa9cd12d3bb34b68d635e1de
ItsJasperWang <jzwang20.thu@vip.163.com>
2025-02-21 01:08:18 -0500

    add public

---
+++ src/components/pages/BlankPage.vue
@@ -0,0 +1,16 @@
+<template>
+<p></p>
+</template>
+<script>
+export default{
+    created(){
+        this.setpath()
+    },
+    methods:{
+        setpath(){
+            this.$router.push("/index/manage/dataset")
+        }
+    }
+}
+</script>
+<style scoped></style>
