f4471eac54038710da27ad5e25506c27ad7383f9
VickyYang25 <keyan@wustl.edu>
2025-04-23 15:40:46 -0400

    fixed reload recommend on click, fixed should return to recommend after upload

--- src/components/pages/Main.vue
+++ src/components/pages/Main.vue
@@ -147,176 +147,185 @@
         </el-table>
       </el-main>
     </el-container>
-  </template>
+</template>
   
-  <script>
-  import Breadcrumb from "../BreadCrumb.vue";
-  
-  export default {
-    name: "MainPage",
-    components: { Breadcrumb },
-    data() {
-      return {
-        currentUser: localStorage.getItem("username") || "",
-        pageType: "",
-        pageTitle: "",
-        fetchedBooks: [],
-        searchQuery: "",
-        transactionType: "",
-        usernameFilter: "",
-        minPrice: null,
-        maxPrice: null,
-        rentDuration: null,
-      };
-    },
-  
-    watch: {
-      '$route.query.pageType': {
-        immediate: true,
-        handler(newVal) {
-          this.loadPage(newVal);
-        },
-      },
-    },
-  
-    methods: {
-      loadPage(type) {
-        console.log("Loading main page of type: " + type);
-        this.pageType = this.$route.query.pageType;
-        this.renderPageTitle();
-        this.fetchBooks();
-      },
-  
-      fetchBooks() {
-        const url = `http://localhost:5001/api/books/${this.pageType}/${this.currentUser}`;
-        fetch(url, {
-          method: "GET",
-          headers: { "content-type": "application/json" },
-        })
-          .then((r) => r.json())
-          .then((data) => {
-            if (data.success) {
-              this.fetchedBooks = data.books;
-            }
-          })
-          .catch(console.error);
-      },
-  
-      searchBooks() {
-        const noParams =
-          !this.searchQuery.trim() &&
-          !this.transactionType &&
-          !this.usernameFilter.trim() &&
-          this.minPrice == null &&
-          this.maxPrice == null &&
-          this.rentDuration == null;
+<script>
+import Breadcrumb from "../BreadCrumb.vue";
 
-        if (noParams) {
-          return this.fetchBooks();
-        }
-                
-        const params = new URLSearchParams();
-        if (this.searchQuery.trim()) {
-          params.append("book_name", this.searchQuery.trim());
-        }
-        if (this.transactionType) {
-          params.append("transaction_type", this.transactionType);
-        }
-        if (this.usernameFilter.trim()) {
-          params.append("username", this.usernameFilter.trim());
-        }
-        if (this.minPrice !== null) {
-          params.append("min_price", this.minPrice);
-        }
-        if (this.maxPrice !== null) {
-          params.append("max_price", this.maxPrice);
-        }
-        if (this.rentDuration !== null) {
-          params.append("rent_duration", this.rentDuration);
-        }
-  
-        const url = `http://localhost:5001/api/books/search?${params.toString()}`;
-        fetch(url, {
-          method: "GET",
-          headers: { "content-type": "application/json" },
-        })
-          .then((r) => r.json())
-          .then((data) => {
-            if (data.success) {
-              this.fetchedBooks = data.books;
-            } else {
-              this.$message.warning(data.message || "Search failed");
-            }
-          })
-          .catch((err) => {
-            console.error(err);
-            this.$message.error("Error while searching");
-          });
-      },
-  
-      formatDate(datetime) {
-        return datetime ? datetime.split("T")[0] : "";
-      },
-  
-      renderPageTitle() {
-        switch (this.pageType) {
-          case "nearby":
-            this.pageTitle = "Books From Your School";
-            break;
-          case "following":
-            this.pageTitle = "Books From People You Are Following";
-            break;
-          default:
-            this.pageTitle = "Books Recommended For You";
-        }
-      },
-  
-      toUpload() {
-        this.$router.push("/upload");
-      },
-  
-      goToUploaderProfile(username) {
-        this.$router.push(`/profile/${username}`);
-      },
-  
-      buyBook(book) {
-        this.$router.push({
-          path: "/transactionConfirm",
-          query: {
-            book_id: book.id,
-            book_name: book.name,
-            book_price: book.price,
-            book_owner: book.owner,
-            book_uploaded: book.uploaded_at,
-            transaction_type: "buy",
-          },
-        });
-      },
-  
-      rentBook(book) {
-        this.$router.push({
-          path: "/transactionConfirm",
-          query: {
-            book_id: book.id,
-            book_name: book.name,
-            book_price: book.price,
-            book_owner: book.owner,
-            book_uploaded: book.uploaded_at,
-            book_rent_duration: book.rent_duration,
-            transaction_type: "rent",
-          },
-        });
+export default {
+  name: "MainPage",
+  components: { Breadcrumb },
+  data() {
+    return {
+      currentUser: localStorage.getItem("username") || "",
+      pageType: "",
+      pageTitle: "",
+      fetchedBooks: [],
+      searchQuery: "",
+      transactionType: "",
+      usernameFilter: "",
+      minPrice: null,
+      maxPrice: null,
+      rentDuration: null,
+    };
+  },
+
+  watch: {
+    '$route.fullPath': {
+      immediate: true,
+      handler() {
+        this.searchQuery = "";
+        this.transactionType = "";
+        this.usernameFilter = "";
+        this.minPrice = null;
+        this.maxPrice = null;
+        this.rentDuration = null;
+
+        this.loadPage(this.$route.query.pageType);
       },
     },
-  };
-  </script>
-  
-  <style scoped>
-  .el-header {
-    height: 56px;
-  }
-  .el-divider {
-    margin: 0;
-  }
-  </style>
+  },
+
+  methods: {
+    loadPage(type) {
+      console.log("Loading main page of type: " + type);
+      this.pageType = this.$route.query.pageType !== undefined 
+      ? this.$route.query.pageType 
+      : "recommend";
+      this.renderPageTitle();
+      this.fetchBooks();
+    },
+
+    fetchBooks() {
+      const url = `http://localhost:5001/api/books/${this.pageType}/${this.currentUser}`;
+      fetch(url, {
+        method: "GET",
+        headers: { "content-type": "application/json" },
+      })
+        .then((r) => r.json())
+        .then((data) => {
+          if (data.success) {
+            this.fetchedBooks = data.books;
+          }
+        })
+        .catch(console.error);
+    },
+
+    searchBooks() {
+      const noParams =
+        !this.searchQuery.trim() &&
+        !this.transactionType &&
+        !this.usernameFilter.trim() &&
+        this.minPrice == null &&
+        this.maxPrice == null &&
+        this.rentDuration == null;
+
+      if (noParams) {
+        return this.fetchBooks();
+      }
+              
+      const params = new URLSearchParams();
+      if (this.searchQuery.trim()) {
+        params.append("book_name", this.searchQuery.trim());
+      }
+      if (this.transactionType) {
+        params.append("transaction_type", this.transactionType);
+      }
+      if (this.usernameFilter.trim()) {
+        params.append("username", this.usernameFilter.trim());
+      }
+      if (this.minPrice !== null) {
+        params.append("min_price", this.minPrice);
+      }
+      if (this.maxPrice !== null) {
+        params.append("max_price", this.maxPrice);
+      }
+      if (this.rentDuration !== null) {
+        params.append("rent_duration", this.rentDuration);
+      }
+
+      const url = `http://localhost:5001/api/books/search?${params.toString()}`;
+      fetch(url, {
+        method: "GET",
+        headers: { "content-type": "application/json" },
+      })
+        .then((r) => r.json())
+        .then((data) => {
+          if (data.success) {
+            this.fetchedBooks = data.books;
+          } else {
+            this.$message.warning(data.message || "Search failed");
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+          this.$message.error("Error while searching");
+        });
+    },
+
+    formatDate(datetime) {
+      return datetime ? datetime.split("T")[0] : "";
+    },
+
+    renderPageTitle() {
+      switch (this.pageType) {
+        case "nearby":
+          this.pageTitle = "Books From Your School";
+          break;
+        case "following":
+          this.pageTitle = "Books From People You Are Following";
+          break;
+        default:
+          this.pageTitle = "Books Recommended For You";
+      }
+    },
+
+    toUpload() {
+      this.$router.push("/upload");
+    },
+
+    goToUploaderProfile(username) {
+      this.$router.push(`/profile/${username}`);
+    },
+
+    buyBook(book) {
+      this.$router.push({
+        path: "/transactionConfirm",
+        query: {
+          book_id: book.id,
+          book_name: book.name,
+          book_price: book.price,
+          book_owner: book.owner,
+          book_uploaded: book.uploaded_at,
+          transaction_type: "buy",
+        },
+      });
+    },
+
+    rentBook(book) {
+      this.$router.push({
+        path: "/transactionConfirm",
+        query: {
+          book_id: book.id,
+          book_name: book.name,
+          book_price: book.price,
+          book_owner: book.owner,
+          book_uploaded: book.uploaded_at,
+          book_rent_duration: book.rent_duration,
+          transaction_type: "rent",
+        },
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.el-header {
+  height: 56px;
+}
+.el-divider {
+  margin: 0;
+}
+</style>
   
